9942bd5f6609f8726a9978eab54d4f3b5e2e132d
kaustubhkx <kaustubhkrishna2003@gmail.com>
2022-12-29 08:42:33 +0530

    Added Comments and added user-input for download location

--- playlist-download.py
+++ playlist-download.py
@@ -5,8 +5,12 @@ get_user_input_link = input("Enter The Playlist URL:- ")
 
 Youtube_playlist = Playlist(get_user_input_link)
 
+#You can find the location in your file explorer [Example:- (D:\Videos)]
+
+download_location = input("Enter the location where you want to download the videos:- ")
+
 for video in Youtube_playlist.videos:
-    video.streams.get_highest_resolution().download("D:\Videos\Javascript-2.0")
+    video.streams.get_highest_resolution().download(download_location)
     print("Video Downloaded", video.title)
 
 print("Playlist Downloaded")
