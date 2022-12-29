07f5b23719e394448d0c83c440077d7ef986100a
kaustubhkx <kaustubhkrishna2003@gmail.com>
2022-12-29 09:41:15 +0530

    Added Comments and fixed some bugs

--- playlist-download.py
+++ playlist-download.py
@@ -1,3 +1,5 @@
+#Make sure the YouTube Playlist is set to Public or Unlisted
+
 from pytube import Playlist
 
 get_user_input_link = input("Enter The Playlist URL:- ")
@@ -6,17 +8,23 @@ Youtube_playlist = Playlist(get_user_input_link)
 
 download_location = str(input("Enter the location where you want to download the videos:- "))
 
-resolution = input("Resolution:- Press 1 for Low Resolution and 2 for High Resolution:- ")
+video_resolution = int(input("Resolution:- Press 1 for High Resolution and Press 2 for Low Resolution:- "))
 
 #You can find the location in your file explorer [Example:- (D:\Videos)]
 
-if resolution==1:
+if video_resolution==1:
     for video in Youtube_playlist.videos:
+        print("Playlist Downloading...")
         video.streams.get_highest_resolution().download(download_location)
         print("Video Downloaded", video.title)
-else:
+        print("Your Playlist has been downloaded")
+elif video_resolution==2:
     for video in Youtube_playlist.videos:
+        print("Playlist Downloading...")
         video.streams.get_lowest_resolution().download(download_location)
         print("Video Downloaded", video.title)
+        print("Your Playlist has been downloaded")
+else:
+    print("The Playlist was not downloaded try again!")
 
-print("Playlist Downloaded")
+
