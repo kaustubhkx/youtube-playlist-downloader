73b12e98d328589de68b7169e7593ab69e76a513
kaustubhkx <kaustubhkrishna2003@gmail.com>
2022-12-29 09:11:21 +0530

    Added more user-inputs for download location and resolution

--- playlist-download.py
+++ playlist-download.py
@@ -1,16 +1,22 @@
 from pytube import Playlist
 
-
 get_user_input_link = input("Enter The Playlist URL:- ")
 
 Youtube_playlist = Playlist(get_user_input_link)
 
+download_location = str(input("Enter the location where you want to download the videos:- "))
+
+resolution = input("Resolution:- Press 1 for Low Resolution and 2 for High Resolution:- ")
+
 #You can find the location in your file explorer [Example:- (D:\Videos)]
 
-download_location = input("Enter the location where you want to download the videos:- ")
-
-for video in Youtube_playlist.videos:
-    video.streams.get_highest_resolution().download(download_location)
-    print("Video Downloaded", video.title)
+if resolution==1:
+    for video in Youtube_playlist.videos:
+        video.streams.get_highest_resolution().download(download_location)
+        print("Video Downloaded", video.title)
+else:
+    for video in Youtube_playlist.videos:
+        video.streams.get_lowest_resolution().download(download_location)
+        print("Video Downloaded", video.title)
 
 print("Playlist Downloaded")
